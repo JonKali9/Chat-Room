ba5402eecf548318f3a2fe98c0a2f2f10624b572
Anton Devi <anton-devi@localhost.localdomain>
2021-04-27 00:18:22 +0200

    Finished adding JS

--- script.js
+++ script.js
@@ -1,2 +1,23 @@
 const messagesDiv = document.getElementById('messages');
-const nicklem = document
+const nickElem = document.querySelector('input.nick')
+const msgElem = document.querySelector('input.msg')
+const sendButton = document.querySelector('button')
+
+const addMsg = () => {
+    const msg = document.createElement('div');
+    msg.classList = 'message';
+    const nick = document.createElement('span');
+    nick.classList = 'nick';
+    nick.innerHTML = nickElem.value + ': ';
+    msg.appendChild(nick);
+    const msgText = document.createElement('span');
+    msgText.classList = 'msg';
+    msgText.innerHTML = msgElem.value;
+    msg.appendChild(msgText);
+    messagesDiv.appendChild(msg);
+}
+
+sendButton.addEventListener('click', addMsg);
+document.addEventListener('keyup', event => {
+    if (event.keyCode == 13) addMsg();
+});
